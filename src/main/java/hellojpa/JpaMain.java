77f2b249937b6949891ea980eb37aacd29d10f68
HarryBae1011 <06harry@naver.com>
2024-11-07 21:13:44 +0900

    JPA 기본 기능

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,10 +7,48 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        //트랜잭션 시작
+        tx.begin();
+
+        try {
+            //멤버 데이터 저장
+            /*
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            em.persist(member);
+             */
+
+            //멤버 데이터 조회
+            /*
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+             */
+
+            //멤버 데이터 수정
+            /*Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+             */
+
+            //멤버 데이터 삭제
+            /*
+            Member findMember = em.find(Member.class, 1L);
+            em.remove(findMember);
+             */
+
+            //트랜잭션 종료
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
