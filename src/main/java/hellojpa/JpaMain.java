bb522f12d5e3d7fa4fe83543324cf7cfb9fda10e
HarryBae1011 <06harry@naver.com>
2024-11-11 23:10:51 +0900

    단방향 연관 관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -42,7 +42,43 @@ public class JpaMain {
             em.remove(findMember);
              */
 
-            //트랜잭션 종료
+            /*
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
+             */
+
+            /*
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            //쓰기 지연 SQL 저장소에 저장
+            em.persist(member1);
+            em.persist(member2);
+             */
+
+            /*
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+             */
+
+            //이 때 DB로 쿼리들이 flush 됨
+            //tx.commit();
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam.getName() = " + findTeam.getName());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,19 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
-    private String name;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -18,11 +23,19 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
